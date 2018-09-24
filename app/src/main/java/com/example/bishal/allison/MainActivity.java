b735243af5e9698d3397d3dbb29a11c0d75c21a4
bishalbaaniya123 <071bct513@pcampus.edu.np>
2018-09-24 08:34:24 +0545

    first push

---
+++ app/src/main/java/com/example/bishal/allison/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.bishal.allison;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Layout
+        RelativeLayout buckysLayout = new RelativeLayout(this);
+        buckysLayout.setBackgroundColor(Color.GREEN);
+
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText("Log In");
+        redButton.setBackgroundColor(Color.RED);
+
+        //Username input
+        EditText username = new EditText(this);
+
+        redButton.setId(1);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //Give rules to position widgets
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+
+        //Add widget to layout (button is now a child of layout)
+        buckysLayout.addView(redButton, buttonDetails);
+        buckysLayout.addView(username, usernameDetails);
+
+        //Set this activity content/display to this view
+        setContentView(buckysLayout);
+    }
+}
